d2c17b2ebe41edf50c7fd296456c3f42f78267ce
msx752 <aslim@outlook.com.tr>
2017-07-16 11:57:46 +0300

    tournamentApi

---
+++ RiotGamesApi.AspNetCore.RiotApi/TournamentEndPoints/LobbyEventDTOWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace RiotGamesApi.AspNetCore.RiotApi.TournamentEndPoints
+{
+    public class LobbyEventDTOWrapper
+    {
+        [JsonProperty("eventList")]
+        public List<LobbyEventDTO> eventList { get; set; }
+    }
+}
---
+++ RiotGamesApi.AspNetCore.RiotApi/TournamentEndPoints/SummonerIdParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace RiotGamesApi.AspNetCore.RiotApi.TournamentEndPoints
+{
+    public class SummonerIdParams
+    {
+        //the tournament participants
+        [JsonProperty("participants")]
+        public List<long> participants { get; set; }
+    }
+}
